2bbcbaea7fd9ce55c11c7aa314b096f9a3c12445
Nagihan Samut <79144070+Nagihansamt@users.noreply.github.com>
2021-05-31 00:33:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayı;
+            Console.WriteLine("bir sayı girin: ");
+            sayı = Convert.ToInt16(Console.ReadLine());
+            while (sayı % 2 == 0)
+            {
+
+            }
+
+            Console.WriteLine("bir sayı giriniz: ");
+            sayı = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("tek sayı girdiniz");
+            Console.ReadLine();
+        }
+
+    }
+
+
+            }
+             
+        
+     
