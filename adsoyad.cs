55428c4bb2728ac265e8559df9b20a45ddaa9978
Nagihan Samut <79144070+Nagihansamt@users.noreply.github.com>
2021-06-03 14:48:22 +0300

    Add files via upload

---
+++ adsoyad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("kullanıcı ismini giriniz:");
+             string isim = Console.ReadLine();
+
+            Console.WriteLine("kullanıcı soyismini giriniz: ");
+            string soyisim = Console.ReadLine();
+
+            Console.WriteLine("kullanıcı ismi: " + isim);
+            Console.WriteLine("kullanıcı ismi: " + soyisim);
+            Console.ReadLine();
+        
+
+            {
+
+            }
+
+
+        }
+    }
+}
