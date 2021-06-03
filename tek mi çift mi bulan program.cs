10fc2c7f2b4ad0ccce5526d039448f22a0bfe200
Nagihan Samut <79144070+Nagihansamt@users.noreply.github.com>
2021-06-03 16:28:41 +0300

    Add files via upload

---
+++ tek mi çift mi bulan program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Bir sayı giriniz: ");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+
+            if (sayı % 2 == 0) 
+            {
+           
+                Console.WriteLine("sayı çifttir");
+            }
+            
+
+         else
+            {
+                Console.WriteLine("sayı tektir");
+            }
+
+
+        }
+    }
+}
