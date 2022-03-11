4f8b81909dace9c6c80cba24f8aa6f143d5aba24
Nagihan Samut <79144070+Nagihansamt@users.noreply.github.com>
2022-03-11 17:42:33 +0300

    Add files via upload

---
+++ ifelse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time <= 11)
+            {
+                Console.WriteLine("Gunaydın");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("iyi gunler");
+            }
+            else
+            {
+             Console.WriteLine("iyi geceler");
+            }
+            Console.ReadLine();
+        }
+    }
+}
