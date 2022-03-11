9524221f04857895a35f2202b8cee0c9a022a577
Nagihan Samut <79144070+Nagihansamt@users.noreply.github.com>
2022-03-11 22:38:03 +0300

    Add files via upload

---
+++ Tersten Metin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmaWor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metin Giriniz");
+            string metin = Console.ReadLine();
+
+            Console.WriteLine("Metninizin Ters Hali");
+            for(int i = metin.Length; i >= 0; i--)
+            {
+                Console.Write(metin[i]);
+            }
+            Console.ReadLine();
+        }
+         
+
+          
+
+            
+        }
+    }
+
