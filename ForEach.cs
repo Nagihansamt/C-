970178281c45a7fc989c591be3aef8e21beb4225
Nagihan Samut <79144070+Nagihansamt@users.noreply.github.com>
2022-03-11 18:50:53 +0300

    Add files via upload

---
+++ ForEach.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+  
+
+namespace While_Foreach
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Sayı giriniz :");
+                int a = int.Parse(Console.ReadLine());
+                int b = 1;
+                int c = 0;
+                while (b <= a)
+                {
+                    c += b;
+                    b++;
+                }
+                Console.WriteLine(c / a);
+                Console.WriteLine("--------------------------");
+                char e = 'a';
+                while (e < 'z')
+                {
+                    Console.WriteLine(e);
+                    e++;
+
+                }
+                Console.WriteLine("--------------------------");
+                string[] ogrenciler = { "Ali", "Ahmet", "Ayşe", "Aylin" };
+
+                foreach (var i in ogrenciler)
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            }
+        }
+    }
+    }
+   
+
